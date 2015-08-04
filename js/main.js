c70b02d24739fe89509924f2ccf6c9f49083b38a
Xenia Tay <xeniatzy@gmail.com>
2015-08-04 00:09:21 -0400

    top bar and header nav done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@
 $(document).ready(function() {
 
   // initialize parallax scroll
-  parallaxScroll.init();
+  // parallaxScroll.init();
 
+  // $('ul.nav li.dropdown').hover(function(){
+  //   $('.dropdown-toggle', this).trigger('click');
+  // });
+
+
+  function bindNavbar() {
+    if ($(window).width() > 768) {
+      $('.extempra-nav .dropdown').on('mouseover', function(){
+        $('.dropdown-toggle', this).next('.dropdown-menu').show();
+      }).on('mouseout', function(){
+        $('.dropdown-toggle', this).next('.dropdown-menu').hide();
+      });
+    }
+    else {
+      $('.extempra-nav .dropdown').off('mouseover').off('mouseout');
+    }
+  }
+
+  $(window).resize(function() {
+    bindNavbar();
+  });
+
+  bindNavbar();
 });
