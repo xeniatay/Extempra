a4fdca708736ee94d61112bfc05e0929bf1e7f24
Xenia Tay <xeniatzy@gmail.com>
2015-09-14 23:41:05 +0800

    Details on homepage

--- js/main.js
+++ js/main.js
@@ -5,27 +5,33 @@ $(document).ready(function() {
   // initialize parallax scroll
   // parallaxScroll.init();
 
-  // $('ul.nav li.dropdown').hover(function(){
-  //   $('.dropdown-toggle', this).trigger('click');
-  // });
-
-
-  function bindNavbar() {
-    if ($(window).width() > 768) {
-      $('.extempra-nav .dropdown').on('mouseover', function(){
-        $('.dropdown-toggle', this).next('.dropdown-menu').show();
-      }).on('mouseout', function(){
-        $('.dropdown-toggle', this).next('.dropdown-menu').hide();
-      });
-    }
-    else {
-      $('.extempra-nav .dropdown').off('mouseover').off('mouseout');
-    }
-  }
-
   $(window).resize(function() {
     bindNavbar();
   });
 
   bindNavbar();
+  expandOfferings();
 });
+
+function bindNavbar() {
+  if ($(window).width() > 768) {
+    $('.extempra-nav .dropdown').on('mouseover', function(){
+      $('.dropdown-toggle', this).next('.dropdown-menu').show();
+    }).on('mouseout', function(){
+      $('.dropdown-toggle', this).next('.dropdown-menu').hide();
+    });
+  }
+  else {
+    $('.extempra-nav .dropdown').off('mouseover').off('mouseout');
+  }
+}
+
+function expandOfferings() {
+  $('#view-all-offerings').click(function() {
+    $(this).toggleClass('expanded');
+    $('.sub-offerings-hidden')
+      .hide()
+      .toggleClass('hide')
+      .fadeIn();
+  });
+}
